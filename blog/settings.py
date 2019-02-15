866e026ae58ff1734bb6ab2e8e70d824e37be75c
kakawkaization <kakawkaization@gmail.com>
2019-02-16 00:14:18 +0300

    changed database

--- blog/settings.py
+++ blog/settings.py
@@ -74,14 +74,21 @@ WSGI_APPLICATION = 'blog.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.postgresql',
+#        'NAME': 'blog',
+#        'USER': 'blog_user',
+#        'PASSWORD': 'qweASD123',
+#        'HOST': '127.0.0.1',
+#        'PORT': '5432',
+##    }
+#}
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'blog',
-        'USER': 'blog_user',
-        'PASSWORD': 'qweASD123',
-        'HOST': '127.0.0.1',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
